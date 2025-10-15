adec502e2182b40cd07597a37b9adf197f353b25
Nischay Mittal <76200599+mnischay2@users.noreply.github.com>
2025-10-15 09:11:29 +0530

    minor changes.
    works till now. just a minor bug in speaker mic sync

--- port_config.py
+++ port_config.py
@@ -3,7 +3,10 @@ central= 5000
 ai_handler= 5001
 
 transcriber= 5002
+
 ui_client= 5003
+
+session_mgr= 5004
 
 speaker= 5005
 speaker_status= 5006
--- shut.py
+++ shut.py
@@ -1,7 +1,7 @@
 import subprocess
 
 files_list = [
-    "central.py", "speaker.py", "ai_handler.py","transcribe.py", "mic.py", "ui_client.py"
+    "central.py", "speaker.py", "ai_handler.py","transcribe.py", "mic.py", "ui_client.py","session_mgr.py"
 ]
 
 for script in files_list:
